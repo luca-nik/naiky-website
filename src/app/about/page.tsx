ba439940270e94b43e0e98625a00b468fbde4106
luca-nik <luca.nicoli.engineer@gmail.com>
2025-01-08 12:39:16 +0100

    Personal description

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -14,24 +14,53 @@ const AboutPage = () => {
         <div className="about-description">
           <h1>About Me</h1>
           <p>
-            Hello! I&#39;m Luca, a passionate developer, thinker, and creator. I enjoy solving 
+            Hello! I&#39;m Luca, a passionate developer, thinker, creator, and alpinist. I enjoy solving 
             complex problems and sharing my experiences through blogs and projects. 
             Explore this space to know more about my journey!
           </p>
+          <p>
+            I&#39;m a Physics Engineer and PhD candidate in Theoretical & Computational Chemistry at Scuola Normale Superiore. My journey has taken me from frying molecules with ultrafast lasers to watching CPUs grinding Quantum Mechanical simulations. 
+            Now, I&#39;m diving into the world of blockchain technology, eager to build Web3 applications.
+          </p>
+          <p>
+            Previously, I worked as an MEV Scientist Engineer at <a href="https://www.urani.trade/" target="_blank" rel="noopener noreferrer">Urani</a>, specializing in toxic MEV reduction on Solana through <a href="https://github.com/urani-trade/solana-mev-agent-py" target="_blank" rel="noopener noreferrer">intent-matching and optimal routing algorithms</a>.
+          </p>
         </div>
-      </div>
 
-      {/* Social Links Section */}
-      <div className="social-links">
-        <div className="github-social-link">
-          <a href="https://github.com/luca-nik" target="_blank" rel="noopener noreferrer">
-            <img src="/_images/github-thumbnail.png" alt="GitHub" />
-          </a>
+        {/* Divider */}
+        <hr />
+
+        {/* Education Section */}
+        <div className="about-education">
+          <h2>Education</h2>
+          <p>PhD in Theoretical & Computational Chemistry | MS & BS in Engineering Physics</p>
         </div>
-        <div className="linkedin-social-link">
-          <a href="https://www.linkedin.com/in/luca-nicoli/" target="_blank" rel="noopener noreferrer">
-            <img src="/_images/linkedin-thumbnail.png" alt="LinkedIn" />
-          </a>
+
+        {/* Divider */}
+        <hr />
+
+        {/* Achievements Section */}
+        <div className="about-achievements">
+          <h2>Achievements</h2>
+          <p><a href="https://scholar.google.com/citations?user=SsTwaqEAAAAJ&hl=en&oi=ao" target="_blank" rel="noopener noreferrer">8 peer-reviewed journal articles</a>, 3 international conference presentations, 1 Italian National patent.</p>
+          <p>Founder of <a href="https://sites.google.com/view/matwins/home" target="_blank" rel="noopener noreferrer">MatTwins</a>, an initiative leveraging surrogate Machine Learning models to reduce the computational complexity of the discovery of novel materials.</p>
+        </div>
+
+        {/* Divider */}
+        <hr />
+
+        {/* Social Links Section */}
+        <div className="social-links">
+          <div className="github-social-link">
+            <a href="https://github.com/luca-nik" target="_blank" rel="noopener noreferrer">
+              <img src="/_images/github-thumbnail.png" alt="GitHub" />
+            </a>
+          </div>
+          <div className="linkedin-social-link">
+            <a href="https://www.linkedin.com/in/luca-nicoli/" target="_blank" rel="noopener noreferrer">
+              <img src="/_images/linkedin-thumbnail.png" alt="LinkedIn" />
+            </a>
+          </div>
         </div>
       </div>
     </div>
