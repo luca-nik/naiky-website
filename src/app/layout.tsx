8b9655b08be2be26c64ccb345f856ea00443aa14
luca-nik <luca.nicoli.engineer@gmail.com>
2025-01-10 13:23:09 +0100

    metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,17 +4,19 @@ import '@/styles/GlobalStyles.css';
 import Navbar from '@/components/Navbar';
 import { Metadata } from 'next';
 
+const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000';
+
 export const metadata: Metadata = {
   title: 'Naiky.xyz',
   description: "A personal blog to share experiences and thoughts.",
   openGraph: {
     title: "Naiky's Personal Blog",
     description: "A personal blog to share experiences and thoughts.",
-    url: 'https://naiky-blog.vercel.app',
+    url: `${baseUrl}`,
     siteName: "Naiky's Personal Blog",
     images: [
       {
-        url: 'https://naiky-blog.vercel.app/_images/luca-nik.jpeg',
+        url: `${baseUrl}/_images/luca-nik.jpeg`,
         width: 1200,
         height: 630,
         alt: 'Naiky Blog Preview Image',
@@ -25,7 +27,7 @@ export const metadata: Metadata = {
     card: 'summary_large_image',
     title: "Naiky's Personal Blog",
     description: "A personal blog to share experiences and thoughts.",
-    images: ['https://naiky-blog.vercel.app/_images/luca-nik.jpeg'],
+    images: [`${baseUrl}/_images/luca-nik.jpeg`],
   },
 };
 
